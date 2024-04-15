e17a56607c4f822771c8fe098cab916780cfe497
benoitbMTL <benoit@buonassera.fr>
2024-04-14 22:28:40 -0400

    layout

--- main/upload.php
+++ main/upload.php
@@ -83,8 +83,8 @@ function listUploads($dir) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <div class="container mt-5">
-        <h1 class="mb-3">File Upload Form</h1>
+<div class="container mt-4 max-width-container" style="padding-top: 75px;">
+        <h3>File Upload Form</h3>
         <form action="upload.php" method="post" enctype="multipart/form-data" class="mb-3">
             <div class="mb-3">
                 <label for="fileToUpload" class="form-label">Select file to upload:</label>
